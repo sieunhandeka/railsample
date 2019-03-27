6f7e3fb53e1140bff98c8789ff74e0748946dc75
SonDoann <sonb17031997@gmail.com>
2019-03-27 02:09:38 +0000

    new image maker

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :images
+  resources :images
+  resources :images
+  resources :images
+  resources :images
   root 'static_pages#home'
 
   devise_for :users
